a5cb3f0fbd6016a8b15a31e3f47c8e6cba3757fc
ADMIN <harshagg75@gmail.com>
2023-04-11 11:17:22 +0530

    program to extract image of object from webcam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import glob
 import time
 from emailing import send_email
 
@@ -7,6 +8,7 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 while True:
     status = 0
     check1, frame1 = video.read()
@@ -14,7 +16,7 @@ while True:
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
     if first_frame is None:
-        first_frame=gray_frame_gau
+        first_frame= gray_frame_gau
 
     delta_frame = cv2.absdiff(first_frame,gray_frame_gau)
 
@@ -31,12 +33,18 @@ while True:
         rectangle = cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0))
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png",frame1)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] ==0:
-        send_email()
+        send_email(image_with_object)
     print(status_list)
 
     cv2.imshow("Video", frame1)
